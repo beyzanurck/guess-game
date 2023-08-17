03074dc28a686f7500dee48f03a993d1e34db003
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-17 09:49:34 -0500

    gets the value of input

--- src/Guess.jsx
+++ src/Guess.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Guess(props) {
+    const [guess, setGuess] = useState('')
+
+    const handleChange = (event) => {
+        setGuess(event.target.value)
+    }
+
   return (
     <div>
       <p>Guess a number between 0-100.</p>
@@ -11,9 +18,11 @@ export default function Guess(props) {
             id='user-guess'
             name='user-guess'
             placeholder='guess here'
+            onChange={handleChange} //calls whenever the input value changes.
         />
         <button>Click</button>
       </form>
+      <p>{guess}</p>
     </div>
   )
 }
