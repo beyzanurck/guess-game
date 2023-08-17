a9a34c722194da533d5c9e5a7813f0cac4073623
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-17 10:31:17 -0500

    prevents the default event of form submission

--- src/Guess.jsx
+++ src/Guess.jsx
@@ -3,16 +3,23 @@ import { useState } from 'react'
 
 export default function Guess(props) {
     const [guess, setGuess] = useState('')
+    
 
     const handleChange = (event) => {
         setGuess(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+
     }
 
   return (
     <div>
       <p>Guess a number between 0-100.</p>
 
-      <form>
+      <form id="form" type="submit" onSubmit={handleSubmit}>
         <input
             type='number'
             id='user-guess'
